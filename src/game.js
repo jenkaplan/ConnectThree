8f6d664a6ef6edd6e7b33e3f5889c6d62f0ce4b2
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2016-12-14 15:32:30 -0500

    this commit will update the HTML classes and add event listeners

---
+++ src/game.js
@@ -0,0 +1,8 @@
+function addPlayer () {
+
+}
+
+document.querySelector('.column1').addEventListener('click', addPlayer);
+document.querySelector('.column2').addEventListener('click', addPlayer);
+document.querySelector('.column3').addEventListener('click', addPlayer);
+document.querySelector('.column4').addEventListener('click', addPlayer);
