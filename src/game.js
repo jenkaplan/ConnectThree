eacba40c595ee5049f7d2f8e41dec9107a668801
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2016-12-15 16:44:38 -0500

    created proper formula for adding column one player class one ccell at a time

--- src/game.js
+++ src/game.js
@@ -1,35 +1,33 @@
 /* eslint-env jQuery, es6 */
 
 const $cellDivs = $('.cell div');
-let $currentPlayer = $('red');
+let currentPlayer = 'red';
 let $gameBoard = $['c1r1', 'c1r2', 'c1r3', 'c1r4', 'c2r1', 'c2r2', 'c2r3', 'c2r4', 'c3r1', 'c3r2', 'c3r3', 'c3r4', 'c4r1', 'c4r2', 'c4r3', 'c4r4'];
 let $inProgress = true;
 let $cellContents = [];
 let $lowestCellCol1 = c1r4;
-let $c1 = ['#c1r4', '#c1r3', '#c1r2', '#c1r1'];
-
-// function changeColor() {
-//  $('#c1r1').addClass('red');
-// }
+let c1 = ['#c1r4', '#c1r3', '#c1r2', '#c1r1'];
 
 function addPlayer() {
   // console.log('working');
   // let $c1 = ['c1r1', 'c1r2', 'c1r3', 'c1r4'];
-  if ($('#c1r4').hasClass('.red') || $('#c1r4').hasClass('.blue')) {
-    for (let i = 3; i < $c1.length; i - 1) {
-      if ($currentPlayer === 'red') {
-        $('c1[i]').addClass('red');
+  if ($('#c1r4').hasClass('red') || $('#c1r4').hasClass('blue')) {
+      // for (let i = 0; i < c1.length; i++) {
+      if (currentPlayer === 'red') {
+        $(c1[0]).addClass('red');
       } else {
-      $('c1[i]').addClass('blue');
+        $(c1[0]).addClass('blue');
       }
+      c1.shift();
+    // }
+    } else {
+      if (currentPlayer === 'red') {
+        $(c1[0]).addClass('red');
+      } else {
+        $(c1[0]).addClass('blue');
+      }
+      c1.shift();
     }
-  } else {
-    if ($currentPlayer === 'red') {
-        $('#c1r4').addClass('red');
-      } else {
-      $('#c1r4').addClass('blue');
-      }
-  }
 }
 
 // let lowestCellCol1 = c1r4;
