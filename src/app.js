10de864373e5c4ad0645e67d9bca8fbd0fff72cf
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2016-12-16 17:06:33 -0500

    fixed bug in winner function

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ function checkWinPutin() {
         ($('#c2r1').hasClass('putin') && $('#c3r2').hasClass('putin') && $('#c4r1').hasClass('putin')) ||
         ($('#c1r2').hasClass('putin') && $('#c2r3').hasClass('putin') && $('#c3r4').hasClass('putin')) ||
         ($('#c1r1').hasClass('putin') && $('#c2r2').hasClass('putin') && $('#c3r3').hasClass('putin')) ||
+        ($('#c1r4').hasClass('putin') && $('#c2r3').hasClass('putin') && $('#c3r2').hasClass('putin')) ||
         ($('#c4r4').hasClass('putin') && $('#c2r2').hasClass('putin') && $('#c3r3').hasClass('putin'))){
       alert('Putin wins!');
       $(alert).makeItRain();
@@ -60,6 +61,7 @@ function checkWinTrump() {
         ($('#c2r1').hasClass('trump') && $('#c3r2').hasClass('trump') && $('#c4r1').hasClass('trump')) ||
         ($('#c1r2').hasClass('trump') && $('#c2r3').hasClass('trump') && $('#c3r4').hasClass('trump')) ||
         ($('#c1r1').hasClass('trump') && $('#c2r2').hasClass('trump') && $('#c3r3').hasClass('trump')) ||
+        ($('#c1r4').hasClass('trump') && $('#c2r3').hasClass('trump') && $('#c3r2').hasClass('trump')) ||
         ($('#c4r4').hasClass('trump') && $('#c2r2').hasClass('trump') && $('#c3r3').hasClass('trump'))){
       alert('Trump wins!');
       $(alert).makeItRain();
