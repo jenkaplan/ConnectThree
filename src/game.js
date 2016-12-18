72dcb3523eac45b091ce4ea1c185c275964d79fd
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2016-12-17 20:23:05 -0500

    this commit will create classes in game.js file that work

--- src/game.js
+++ src/game.js
@@ -1,76 +1,78 @@
-/* eslint-env jQuery, es6 */
+/* eslint-env jQuery, es6, es5 */
 
 class Game {
-  constructor(totalPlays, putinPlays, trumpPlays, winner, winnerFound) {
-    this.totalPlays = 0;
-    this.putinPlays = [];
-    this.trumpPlays = [];
-    this.winner = null;
-    this.winnerFound = false;
-    // this.currentPlayer = currentPlayer;
+  constructor(currentPlayer) {
+    this.currentPlayer = currentPlayer;
+    this.c1 = ['#c1r4', '#c1r3', '#c1r2', '#c1r1'];
+    this.c2 = ['#c2r4', '#c2r3', '#c2r2', '#c2r1'];
+    this.c3 = ['#c3r4', '#c3r3', '#c3r2', '#c3r1'];
+    this.c4 = ['#c4r4', '#c4r3', '#c4r2', '#c4r1'];
+    this.addPlayerCol1 = this.addPlayerCol1.bind(this);
+    this.addPlayerCol2 = this.addPlayerCol2.bind(this);
+    this.addPlayerCol3 = this.addPlayerCol3.bind(this);
+    this.addPlayerCol4 = this.addPlayerCol4.bind(this);
+  }
+
+  addPlayerCol1(currentPlayer, c1) {
+    if (this.currentPlayer === 'putin') {
+      $(this.c1[0]).addClass('putin');
+      this.currentPlayer = 'trump';
+    } else {
+      $(this.c1[0]).addClass('trump');
+      this.currentPlayer = 'putin';
+    }
+    this.c1.shift();
+    checkWinPutin();
+    checkWinTrump();
+  }
+
+  addPlayerCol2(currentPlayer, c2) {
+    if (this.currentPlayer === 'putin') {
+      $(this.c2[0]).addClass('putin');
+      this.currentPlayer = 'trump';
+    } else {
+      $(this.c2[0]).addClass('trump');
+      this.currentPlayer = 'putin';
+    }
+    this.c2.shift();
+    checkWinPutin();
+    checkWinTrump();
+  }
+
+  addPlayerCol3(currentPlayer, c3) {
+    if (this.currentPlayer === 'putin') {
+      $(this.c3[0]).addClass('putin');
+      this.currentPlayer = 'trump';
+    } else {
+      $(this.c3[0]).addClass('trump');
+      this.currentPlayer = 'putin';
+    }
+    this.c3.shift();
+    checkWinPutin();
+    checkWinTrump();
+  }
+
+  addPlayerCol4(currentPlayer, c4) {
+    if (this.currentPlayer === 'putin') {
+      $(this.c4[0]).addClass('putin');
+      this.currentPlayer = 'trump';
+    } else {
+      $(this.c4[0]).addClass('trump');
+      this.currentPlayer = 'putin';
+    }
+    this.c4.shift();
+    checkWinPutin();
+    checkWinTrump();
   }
 }
 
-let currentPlayer = 'putin';
-let c1 = ['#c1r4', '#c1r3', '#c1r2', '#c1r1'];
-let c2 = ['#c2r4', '#c2r3', '#c2r2', '#c2r1'];
-let c3 = ['#c3r4', '#c3r3', '#c3r2', '#c3r1'];
-let c4 = ['#c4r4', '#c4r3', '#c4r2', '#c4r1'];
+let game = new Game('trump');
+// game.addPlayerCol1();
+// game.addPlayerCol2();
+// game.addPlayerCol3();
+// game.addPlayerCol4();
 
-function addPlayerCol1() {
-  if (currentPlayer === 'putin') {
-    $(c1[0]).addClass('putin');
-    currentPlayer = 'trump';
-  } else {
-    $(c1[0]).addClass('trump');
-    currentPlayer = 'putin';
-  }
-  c1.shift();
-  checkWinPutin();
-  checkWinTrump();
-}
-
-function addPlayerCol2() {
-  if (currentPlayer === 'putin') {
-    $(c2[0]).addClass('putin');
-    currentPlayer = 'trump';
-  } else {
-    $(c2[0]).addClass('trump');
-    currentPlayer = 'putin';
-  }
-  c2.shift();
-  checkWinPutin();
-  checkWinTrump();
-}
-
-function addPlayerCol3() {
-  if (currentPlayer === 'putin') {
-    $(c3[0]).addClass('putin');
-    currentPlayer = 'trump';
-  } else {
-    $(c3[0]).addClass('trump');
-    currentPlayer = 'putin';
-  }
-  c3.shift();
-  checkWinPutin();
-  checkWinTrump();
-}
-
-function addPlayerCol4() {
-  if (currentPlayer === 'putin') {
-    $(c4[0]).addClass('putin');
-    currentPlayer = 'trump';
-  } else {
-    $(c4[0]).addClass('trump');
-    currentPlayer = 'putin';
-  }
-  c4.shift();
-  checkWinPutin();
-  checkWinTrump();
-}
-
-
-$('.column1').on('click', addPlayerCol1);
-$('.column2').on('click', addPlayerCol2);
-$('.column3').on('click', addPlayerCol3);
-$('.column4').on('click', addPlayerCol4);
+$('.column1').on('click', game.addPlayerCol1);
+$('.column2').on('click', game.addPlayerCol2);
+$('.column3').on('click', game.addPlayerCol3);
+$('.column4').on('click', game.addPlayerCol4);
