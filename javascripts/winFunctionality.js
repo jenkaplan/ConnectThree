01329d48f9dda7bb9158a66692014ab19e5c1e1e
Jennifer Kaplan Abowd <jenniferkaplan.nyc@gmail.com>
2019-04-24 11:54:37 -0400

    Updated spacing

--- javascripts/winFunctionality.js
+++ javascripts/winFunctionality.js
@@ -39,19 +39,19 @@ class Win {
       ($('#c2r4').hasClass(leader) && $('#c3r3').hasClass(leader) && $('#c4r2').hasClass(leader)) ||
       ($('#c4r4').hasClass(leader) && $('#c2r2').hasClass(leader) && $('#c3r3').hasClass(leader))) {
         if (leader === 'putin') {
-        alert('Putin wins!');
-        $(alert).makeItRain();
-        // I used this to add the song http://stackoverflow.com/questions/9419263/playing-audio-with-javascript
-        const bond = new Audio('./songs/007.mp3');
-        bond.play();
-        this.winnerFound = true;
+          alert('Putin wins!');
+          $(alert).makeItRain();
+          // I used this to add the song http://stackoverflow.com/questions/9419263/playing-audio-with-javascript
+          const bond = new Audio('./songs/007.mp3');
+          bond.play();
+          this.winnerFound = true;
         } else if (leader === 'trump') {
-            alert('Trump wins!');
-            $(alert).makeItRain();
-            // I used this to add the song http://stackoverflow.com/questions/9419263/playing-audio-with-javascript
-            const bennyHill = new Audio('./songs/BennyHill.mp3');
-            bennyHill.play();
-            this.winnerFound = true;
+          alert('Trump wins!');
+          $(alert).makeItRain();
+          // I used this to add the song http://stackoverflow.com/questions/9419263/playing-audio-with-javascript
+          const bennyHill = new Audio('./songs/BennyHill.mp3');
+          bennyHill.play();
+          this.winnerFound = true;
         }
       } else {
         this.winnerFound = false;
